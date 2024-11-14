34fb0923ff10a0637190dbd5d97d8209eecfa4ff
harshu1210 <charshal40@gmail.com>
2024-11-14 23:34:47 +0530

    Dev-Dojo-Backend

---
+++ devdojobackend/src/main/java/com/devdojo/devdojobackend/DevdojobackendApplication.java
@@ -0,0 +1,13 @@
+package com.devdojo.devdojobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdojobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdojobackendApplication.class, args);
+	}
+
+}
---
+++ devdojobackend/src/test/java/com/devdojo/devdojobackend/DevdojobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdojo.devdojobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdojobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
